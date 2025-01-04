f2ec05471874d50f55edcf03b1cab8922dfd7790
Viktar Hushchynski <viktar.hushchynski@gmail.com>
2025-01-03 21:52:19 -0500

    feat: Move more files from backup

---
+++ DsAlRankToGlory/DsAlRankToGlory/Problems/SortingAndSearching/InsertSort.cs
@@ -0,0 +1,74 @@
+namespace DsAlRankToGlory.Problems.SortingAndSearching;
+
+public class InsertSort
+{
+    static void Sort(int[] array)
+    {
+        for (var i = array.Length - 1; i >= 0; i--)
+        {
+            /* select value to be inserted */
+            var valueToInsert = array[i];
+
+            var holePosition = i;
+
+            /*locate hole position for the element to be inserted */
+
+            while (holePosition > 0 && array[holePosition - 1] > valueToInsert)
+            {
+                array[holePosition] = array[holePosition - 1];
+
+                holePosition -= 1;
+            }
+
+            /* insert the number at hole position */
+            array[holePosition] = valueToInsert;
+
+            foreach (var item in array)
+            {
+                Console.Write("{0} ", item);
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    static void Sort2(int[] ar)
+    {
+        var valueToInsert = ar[^1];
+
+        for (var i = ar.Length - 1; i >= 0; i--)
+        {
+            if (ar[i] > valueToInsert)
+            {
+                ar[i + 1] = ar[i];
+                PrintResult(ar);
+
+                if (i != 0) continue;
+                
+                ar[i] = valueToInsert;
+                PrintResult(ar);
+            }
+            else if (ar[i] < valueToInsert)
+            {
+                ar[i + 1] = valueToInsert;
+                PrintResult(ar);
+                return;
+            }
+            else
+            {
+                ar[i + 1] = valueToInsert;
+                PrintResult(ar);
+            }
+        }
+    }
+
+    private static void PrintResult(int[] array)
+    {
+        foreach (var t in array)
+        {
+            Console.Write("{0} ", t);
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ DsAlRankToGlory/DsAlRankToGlory/Problems/SortingAndSearching/TomAndJerryFirst.cs
@@ -0,0 +1,106 @@
+namespace DsAlRankToGlory.Problems.SortingAndSearching;
+
+public class TomAndJerryFirst
+{
+    static void Run()
+    {
+        var fieldDimension = Console.ReadLine();
+
+        var jerryPosition = Console.ReadLine();
+
+        int xLimit = Convert.ToInt32(fieldDimension?.Split(' ')[0]);
+        int yLimit = Convert.ToInt32(fieldDimension?.Split(' ')[1]);
+
+        int jerryXPosition = Convert.ToInt32(jerryPosition?.Split(' ')[0]) - 1;
+        int jerryYPosition = Convert.ToInt32(jerryPosition?.Split(' ')[1]) - 1;
+
+        var fieldMatrix = new bool[xLimit, yLimit];
+
+        for (int i = 0; i < yLimit; i++)
+        {
+            var line = Console.ReadLine();
+
+            var lineArray = line.Split(' ');
+
+            int x = 0;
+
+            foreach (var item in lineArray)
+            {
+                fieldMatrix[x, i] = Convert.ToBoolean(Convert.ToInt32(item));
+                x += 1;
+            }
+        }
+
+        var tomXPosition = 0;
+        var tomYPosition = 0;
+        var prevX = 0;
+        var prevY = 0;
+
+        while (fieldMatrix[tomXPosition, tomYPosition] == false)
+        {
+            int moveCounter = 0;
+
+            while (true)
+            {
+                var nextMove = MoveForwardCoordinates(fieldMatrix, tomXPosition, tomYPosition, prevX, prevY);
+
+                if (nextMove == null)
+                {
+                    fieldMatrix[tomXPosition, tomYPosition] = true;
+
+                    tomXPosition = 0;
+                    tomYPosition = 0;
+
+                    prevX = 0;
+                    prevY = 0;
+
+                    break;
+                }
+
+                prevX = tomXPosition;
+                prevY = tomYPosition;
+                tomXPosition = nextMove.Item1;
+                tomYPosition = nextMove.Item2;
+
+                moveCounter += 1;
+
+                if (tomXPosition == jerryXPosition && tomYPosition == jerryYPosition)
+                {
+                    Console.WriteLine(moveCounter);
+
+                    return;
+                }
+            }
+        }
+
+        Console.WriteLine(-1);
+    }
+
+    static Tuple<int, int>? MoveForwardCoordinates(bool[,] matrix, int x, int y, int previousX, int previousY)
+    {
+        int xMax = matrix.GetUpperBound(0);
+        int yMax = matrix.GetUpperBound(1);
+
+        if (x < xMax && x + 1 != previousX && matrix[x + 1, y] == false)
+        {
+            return Tuple.Create(x + 1, y);
+        }
+
+        if (y < yMax && y + 1 != previousY && matrix[x, y + 1] == false)
+        {
+            return Tuple.Create(x, y + 1);
+        }
+
+        if (x > 0 && x - 1 != previousX && matrix[x - 1, y] == false)
+        {
+            return Tuple.Create(x - 1, y);
+        }
+
+        if (y > 0 && y - 1 != previousY && matrix[x, y - 1] == false)
+        {
+            return Tuple.Create(x, y - 1);
+        }
+
+        return null;
+    }
+}
